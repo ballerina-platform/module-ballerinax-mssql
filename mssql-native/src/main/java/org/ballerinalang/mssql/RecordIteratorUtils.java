4797810adec40247ecc251e64e150acf0a2913a9
Rajinthan <rajinthan2098@gmail.com>
2021-03-09 11:59:39 +0530

    change client name

--- mssql-native/src/main/java/org/ballerinalang/mssql/RecordIteratorUtils.java
+++ mssql-native/src/main/java/org/ballerinalang/mssql/RecordIteratorUtils.java
@@ -5,7 +5,7 @@ import io.ballerina.runtime.api.values.BObject;
 import org.ballerinalang.mssql.parameterprocessor.MssqlResultParameterProcessor;
 
 public class RecordIteratorUtils {
-    public static Object nextResult(BObject postgresRecordIterator, BObject recordIterator) {
+    public static Object nextResult(BObject mssqlRecordIterator, BObject recordIterator) {
         System.out.println("\nMssql RecordIteratorUtilsn");
         return org.ballerinalang.sql.utils.RecordIteratorUtils.nextResult(recordIterator, MssqlResultParameterProcessor.getInstance());
     }
