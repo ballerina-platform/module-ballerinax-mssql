cddd4c8867f962d757da2aebabaf4aa02772f6ce
Kalaiyarasiganeshalingam <ashakalai31@gmail.com>
2023-06-16 17:44:07 +0530

    Add print statement

--- compiler-plugin-tests/src/test/java/io/ballerina/stdlib/mssql/compiler/CompilerPluginTest.java
+++ compiler-plugin-tests/src/test/java/io/ballerina/stdlib/mssql/compiler/CompilerPluginTest.java
@@ -30,8 +30,10 @@ import io.ballerina.tools.diagnostics.DiagnosticSeverity;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.PrintStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -128,6 +130,8 @@ public class CompilerPluginTest {
                 .filter(r -> r.diagnosticInfo().severity().equals(DiagnosticSeverity.ERROR))
                 .collect(Collectors.toList());
         long availableErrors = diagnosticErrorStream.size();
+        PrintStream asd =System.out;
+        asd.println(Arrays.toString(diagnosticErrorStream.toArray()));
         Assert.assertEquals(availableErrors, 3);
         Assert.assertEquals(diagnosticErrorStream.get(0).diagnosticInfo().messageFormat(),
                 "invalid value: expected value is greater than one");
