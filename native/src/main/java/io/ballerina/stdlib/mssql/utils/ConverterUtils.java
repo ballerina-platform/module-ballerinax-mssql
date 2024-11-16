00d257d72a2a940c36a54c38a45a56a8b58bbb71
ravinperera00 <bravinperera@gmail.com>
2024-11-16 21:20:11 +0530

    Migrate to new Runtime APIs

--- native/src/main/java/io/ballerina/stdlib/mssql/utils/ConverterUtils.java
+++ native/src/main/java/io/ballerina/stdlib/mssql/utils/ConverterUtils.java
@@ -19,7 +19,7 @@
 package io.ballerina.stdlib.mssql.utils;
 
 import com.microsoft.sqlserver.jdbc.Geometry;
-import io.ballerina.runtime.api.TypeTags;
+import io.ballerina.runtime.api.types.TypeTags;
 import io.ballerina.runtime.api.utils.TypeUtils;
 import io.ballerina.runtime.api.values.BArray;
 import io.ballerina.runtime.api.values.BDecimal;
